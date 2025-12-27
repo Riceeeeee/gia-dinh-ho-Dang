0ea4f0a84c6329712f23eba616d0993bcac9976a
Quang <minhquangdang1357@gmail.com>
2025-12-27 22:55:48 +0700

    Sửa lỗi đường dẫn base để hiển thị giao diện

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Thêm dòng base dưới đây để chỉ định đường dẫn cho GitHub Pages
+  base: '/gia-dinh-ho-Dang/', 
 })
